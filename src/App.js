643f704a35c6191b53004762123ecbd261661a15
Carlos Apolaya <temis_it@hotmail.com>
2022-12-03 12:50:52 -0500

    hola

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello everyobdy!
+        comprnando proyectoy!
         </p>
         <a
           className="App-link"
